97a05a3388aa54358832dd4255582ed61790e28a
Val Iacovone <vale.iacovone@gmail.com>
2018-08-26 17:44:11 -0300

    Modulo 2 - Programa

---
+++ Program_Modulo2.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Ejercicio 2 Funciona
+            //Console.WriteLine(200 + 56);
+            // Ejercicio 3 Funciona
+            //Console.WriteLine(13 * 12);
+            // Ejercicio 4 Funciona
+            //Console.WriteLine(321 - 213);
+            // Ejercicio 5 Funciona
+            //Console.WriteLine((20+5)%6);
+            // Ejercicio 6 Funciona
+            //int Multiplicando = 12;
+            //int Multiplicador = 102;
+            //Console.WriteLine(Multiplicando * Multiplicador);
+            // Ejercicio 7 Funciona
+            //int Sumando_1 = 200;
+            //int Sumando_2 = 1111;
+            //Console.WriteLine(Sumando_1 + Sumando_2);
+            // Ejercicio 8 Funciona
+            //int numero1 = 10;
+            //int numero2 = 25;
+            //Console.WriteLine(numero1 * numero2);
+            // Ejercicio 9 Funcionan OK
+            /* Console.WriteLine("Ingrese el número Multiplicador");
+            int int1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el número Multiplicando");
+            int int2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(int1 * int2); */
+            // Ejercicio 10 Funciona OK
+            /*Console.WriteLine("Ingrese un numero entero");
+            int numEntero = Convert.ToInt32(Console.ReadLine());
+            if (numEntero % 2 == 0 )
+                {
+                Console.WriteLine("El numero es par");
+                }
+            else
+                {
+                    Console.WriteLine("El numero es impar");
+                }
+            Console.ReadKey();*/
+            //Ejercicio 11 Funciona OK
+            /*Console.WriteLine("Ingrese el primer numero");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            if (numero1 > numero2 )
+            {
+                Console.WriteLine("El mayor es el primer numero:" + numero1);
+            }
+            else
+            {
+                Console.WriteLine("El mayor es el segundo numero:" + numero2);
+            }
+            Console.ReadKey();*/
+            //Ejercicio 12 Funciona
+            /*Console.WriteLine("Ingrese el primer numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num2 != 0)
+                {
+                    Console.WriteLine(num1 / num2);
+                }
+            else
+                {
+                    Console.WriteLine("Error: No se puede dividir entre cero");
+                }
+            Console.ReadKey(); */
+            //Ejercicio13 funciona
+            /* Console.WriteLine("Ingrese un numero del 1 al 5");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            switch (num1)
+            {
+                case 1:
+                    Console.WriteLine("uno");
+                    break;
+                case 2:
+                    Console.WriteLine("dos");
+                    break;
+                case 3:
+                    Console.WriteLine("tres");
+                    break;
+                case 4:
+                    Console.WriteLine("cuatro");
+                    break;
+                case 5:
+                    Console.WriteLine("cinco");
+                    break;
+                default:
+                    Console.WriteLine("Numero incorrecto");
+                    break;
+            }
+            Console.ReadKey(); */
+            //Ejercicio 14 Funciona
+
+             /* int contnum = 1;
+            while (contnum <= 10)
+            {
+                
+                Console.WriteLine(contnum);
+                contnum = contnum + 1;
+            }
+            Console.ReadKey(); */
+
+            //Ejercicio 15 Funciona
+            /*int num = 1;
+            do
+            {
+                Console.WriteLine(num);
+                num = num + 1;
+
+            } while (num <= 10);
+            Console.ReadKey(); */
+            //Ejercicio 16 Funciona
+            /* int n = 10;
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            Console.ReadKey(); */
+
+            
+
+        }
+    }
+}
